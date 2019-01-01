a15d3fb7068f72be541acd236bb75c14d12997ff
ApoGouv <apo.gouv@gmail.com>
2019-01-01 11:36:05 +0200

    updated sample db

--- db_sample/friends_cube_db.sql
+++ db_sample/friends_cube_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 21, 2018 at 12:04 PM
+-- Generation Time: Jan 01, 2019 at 09:35 AM
 -- Server version: 5.6.37
 -- PHP Version: 5.6.31
 
@@ -198,62 +198,101 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `date_added` datetime NOT NULL,
   `user_closed` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
   `deleted` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `likes` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `likes` int(11) NOT NULL,
+  `image` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `body`, `added_by`, `user_to`, `date_added`, `user_closed`, `deleted`, `likes`) VALUES
-(1, 'This is the FISRT post!', 'apo_gouv_2', 'none', '2018-10-21 19:44:24', 'no', 'no', 0),
-(2, 'This is the 2nd post!', 'apo_gouv_2', 'none', '2018-10-21 19:45:13', 'no', 'no', 0),
-(3, 'This should be the 3rd post!', 'apo_gouv_2', 'none', '2018-10-21 19:46:22', 'no', 'no', 0),
-(8, 'Hey self! -_-', 'apo_gouv_3', 'none', '2018-10-22 20:26:19', 'no', 'no', 0),
-(13, 'Spicy jalapeno bacon ipsum dolor amet turkey pig sirloin ground round corned beef ball tip alcatra tri-tip t-bone venison pancetta sausage cupim bresaola.', 'apo_gouv_2', 'none', '2018-10-22 21:39:45', 'no', 'no', 0),
-(14, 'Jowl pork belly andouille shankle, pastrami frankfurter shank hamburger sirloin bresaola ground round. Sirloin meatball ham porchetta, tail beef ribs rump pig ham hock flank', 'apo_gouv_2', 'none', '2018-10-22 21:39:54', 'no', 'no', 0),
-(15, 'Fatback shank meatloaf porchetta venison pork loin shoulder chuck buffalo pork belly. ', 'apo_gouv_3', 'none', '2018-10-22 21:40:01', 'no', 'no', 0),
-(16, 'Bacon tenderloin t-bone boudin tri-tip pancetta sausage landjaeger chicken doner.', 'apo_gouv_3', 'none', '2018-10-22 21:40:07', 'no', 'no', 0),
-(17, 'Corned beef short loin biltong pork chop.', 'apo_gouv_2', 'none', '2018-10-22 21:40:12', 'no', 'no', 0),
-(18, 'Ham meatloaf tongue salami ribeye doner bresaola hamburger prosciutto filet mignon pastrami strip steak.', 'apo_gouv_3', 'none', '2018-10-22 21:40:19', 'no', 'no', 0),
-(19, 'Tongue tenderloin porchetta biltong jerky ribeye pork sausage short loin salami.', 'apo_gouv_3', 'none', '2018-10-22 21:40:25', 'no', 'no', 0),
-(20, 'Chuck turducken pork frankfurter sirloin short loin, t-bone porchetta kielbasa ham hock. ', 'apo_gouv_2', 'none', '2018-10-22 21:40:33', 'no', 'no', 0),
-(21, 'Drumstick kielbasa fatback, bresaola cupim turkey buffalo alcatra ground round salami corned beef pork chop', 'apo_gouv_2', 'none', '2018-10-22 21:40:40', 'no', 'no', 0),
-(22, 'Chuck beef pork, kevin short ribs flank kielbasa.', 'apo_gouv_2', 'none', '2018-10-22 21:40:49', 'no', 'no', 0),
-(23, 'Pastrami landjaeger ball tip fatback, sausage alcatra chuck kevin boudin ham shank corned beef tail.', 'apo_gouv_2', 'none', '2018-10-22 21:40:56', 'no', 'no', 0),
-(24, 'porchetta shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:02', 'no', 'no', 0),
-(25, 'Fatback shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:03', 'no', 'no', 0),
-(26, 'porchetta shank meatloaf porchetta venison pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:04', 'no', 'no', 0),
-(27, 'porchetta shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:05', 'no', 'no', 0),
-(28, 'Fatback shank meatloaf porchetta meatloaf pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:06', 'no', 'no', 0),
-(29, 'porchetta shank meatloaf porchetta venison pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:07', 'no', 'no', 0),
-(30, 'Fatback shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:08', 'no', 'no', 0),
-(31, 'Fatback meatloaf meatloaf porchetta venison pork loin...', 'apo_gouv_2', 'none', '2018-10-22 21:40:09', 'no', 'no', 0),
-(32, 'meatloaf shank meatloaf meatloaf meatloaf pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:20', 'no', 'no', 0),
-(33, 'Meatball pancetta salami leberkas fatback cupim. Kielbasa prosciutto jerky turkey bacon. Landjaeger drumstick ribeye turducken, pastrami venison short ribs.', 'mickey_mause', 'none', '2018-10-26 12:14:51', 'no', 'no', 0),
-(34, 'Flank chuck short ribs, pork chop capicola venison pork belly pork. Pig prosciutto frankfurter, ribeye alcatra pancetta pork tri-tip.', 'mickey_mause', 'none', '2018-10-26 12:15:01', 'no', 'no', 0),
-(35, 'Rump meatball flank alcatra kielbasa buffalo pork chop shankle pork chuck jerky venison beef ham hock.', 'mickey_mause', 'none', '2018-10-26 12:16:24', 'no', 'no', 0),
-(36, 'Porchetta shank venison shoulder short ribs. Bresaola beef chicken rump filet mignon tenderloin jerky, pork chop burgdoggen beef ribs pork doner spare ribs. Tenderloin sausage bacon ham biltong beef ribs fatback ham hock pork loin.', 'goofy_duck', 'none', '2018-10-26 12:18:23', 'no', 'no', 1),
-(37, 'Meatloaf beef tenderloin ball tip pig boudin spare ribs flank porchetta landjaeger sirloin tri-tip andouille cow brisket. Ham hock flank chuck, pork loin ham hamburger kielbasa boudin meatball. ', 'goofy_duck', 'none', '2018-10-26 12:18:35', 'no', 'no', 1),
-(38, 'Filet mignon frankfurter kevin tongue tenderloin short ribs cupim meatball pig picanha doner tri-tip.', 'mike_ross', 'none', '2018-10-26 12:19:38', 'no', 'no', 0),
-(39, 'Pork loin ham hock venison chicken ground round meatloaf tri-tip spare ribs tongue pastrami prosciutto filet mignon tenderloin short ribs meatball. Salami turkey ribeye beef biltong doner tenderloin. ', 'homer_simson', 'none', '2018-10-26 12:19:45', 'no', 'no', 0),
-(40, 'Pancetta short ribs turkey, sausage pork loin alcatra pork chop cow spare ribs kevin. Pastrami hamburger beef ribs, shankle spare ribs biltong pig strip steak pancetta pork loin.', 'mike_ross', 'none', '2018-10-26 12:19:56', 'no', 'no', 1),
-(41, 'Beef ribs strip steak bacon corned beef. Short ribs drumstick filet mignon ground round pork loin. ', 'homer_simson', 'none', '2018-10-26 12:20:05', 'no', 'no', 1),
-(42, 'Pork loin prosciutto alcatra bacon spare ribs burgdoggen rump pig hamburger kevin.', 'mike_ross', 'none', '2018-10-26 12:20:13', 'no', 'no', 0),
-(43, 'Shankle spare ribs pig jowl hamburger leberkas. Corned beef fatback short loin ham. Porchetta shankle drumstick short ribs pastrami. Andouille frankfurter filet mignon turkey shank buffalo.', 'homer_simson', 'none', '2018-10-26 12:20:24', 'no', 'no', 1),
-(44, 'Testarious!\r\n', 'mike_ross', 'none', '2018-11-18 20:03:53', 'no', 'yes', 0),
-(45, 'Hey mike!\r\n', 'goofy_duck', 'mike_ross', '2018-11-18 20:06:20', 'no', 'no', 0),
-(46, 'Tha er8ei notice??', 'goofy_duck', 'none', '2018-12-15 20:17:18', 'no', 'no', 1),
-(47, 'prfile postts', 'goofy_duck', 'none', '2018-12-15 20:18:50', 'no', 'no', 0),
-(48, 'aaaaaaa 3c\r\n', 'goofy_duck', 'homer_simson', '2018-12-15 20:19:41', 'no', 'yes', 0),
-(49, 'Hey Homer', 'goofy_duck', 'homer_simson', '2018-12-15 20:43:18', 'no', 'yes', 0),
-(50, 'Hey Homer', 'goofy_duck', 'homer_simson', '2018-12-15 20:46:10', 'no', 'yes', 0),
-(51, 'Hey Homer!', 'goofy_duck', 'homer_simson', '2018-12-15 20:47:51', 'no', 'yes', 0),
-(52, 'aaaaaa No.5', 'goofy_duck', 'homer_simson', '2018-12-15 20:51:32', 'no', 'yes', 0),
-(53, 'Hey Homer!', 'goofy_duck', 'homer_simson', '2018-12-15 20:52:25', 'no', 'no', 0),
-(54, 'douleuei?? h mas douleuei??', 'goofy_duck', 'mike_ross', '2018-12-15 20:54:38', 'no', 'yes', 0),
-(55, 'MIKE!!!!!!!!!!', 'goofy_duck', 'mike_ross', '2018-12-15 21:05:14', 'no', 'no', 0),
-(56, 'hr8e to notice!!', 'homer_simson', 'goofy_duck', '2018-12-15 21:11:04', 'no', 'no', 0);
+INSERT INTO `posts` (`id`, `body`, `added_by`, `user_to`, `date_added`, `user_closed`, `deleted`, `likes`, `image`) VALUES
+(1, 'This is the FISRT post!', 'apo_gouv_2', 'none', '2018-10-21 19:44:24', 'no', 'no', 0, ''),
+(2, 'This is the 2nd post!', 'apo_gouv_2', 'none', '2018-10-21 19:45:13', 'no', 'no', 0, ''),
+(3, 'This should be the 3rd post!', 'apo_gouv_2', 'none', '2018-10-21 19:46:22', 'no', 'no', 0, ''),
+(8, 'Hey self! -_-', 'apo_gouv_3', 'none', '2018-10-22 20:26:19', 'no', 'no', 0, ''),
+(13, 'Spicy jalapeno bacon ipsum dolor amet turkey pig sirloin ground round corned beef ball tip alcatra tri-tip t-bone venison pancetta sausage cupim bresaola.', 'apo_gouv_2', 'none', '2018-10-22 21:39:45', 'no', 'no', 0, ''),
+(14, 'Jowl pork belly andouille shankle, pastrami frankfurter shank hamburger sirloin bresaola ground round. Sirloin meatball ham porchetta, tail beef ribs rump pig ham hock flank', 'apo_gouv_2', 'none', '2018-10-22 21:39:54', 'no', 'no', 0, ''),
+(15, 'Fatback shank meatloaf porchetta venison pork loin shoulder chuck buffalo pork belly. ', 'apo_gouv_3', 'none', '2018-10-22 21:40:01', 'no', 'no', 0, ''),
+(16, 'Bacon tenderloin t-bone boudin tri-tip pancetta sausage landjaeger chicken doner.', 'apo_gouv_3', 'none', '2018-10-22 21:40:07', 'no', 'no', 0, ''),
+(17, 'Corned beef short loin biltong pork chop.', 'apo_gouv_2', 'none', '2018-10-22 21:40:12', 'no', 'no', 0, ''),
+(18, 'Ham meatloaf tongue salami ribeye doner bresaola hamburger prosciutto filet mignon pastrami strip steak.', 'apo_gouv_3', 'none', '2018-10-22 21:40:19', 'no', 'no', 0, ''),
+(19, 'Tongue tenderloin porchetta biltong jerky ribeye pork sausage short loin salami.', 'apo_gouv_3', 'none', '2018-10-22 21:40:25', 'no', 'no', 0, ''),
+(20, 'Chuck turducken pork frankfurter sirloin short loin, t-bone porchetta kielbasa ham hock. ', 'apo_gouv_2', 'none', '2018-10-22 21:40:33', 'no', 'no', 0, ''),
+(21, 'Drumstick kielbasa fatback, bresaola cupim turkey buffalo alcatra ground round salami corned beef pork chop', 'apo_gouv_2', 'none', '2018-10-22 21:40:40', 'no', 'no', 0, ''),
+(22, 'Chuck beef pork, kevin short ribs flank kielbasa.', 'apo_gouv_2', 'none', '2018-10-22 21:40:49', 'no', 'no', 0, ''),
+(23, 'Pastrami landjaeger ball tip fatback, sausage alcatra chuck kevin boudin ham shank corned beef tail.', 'apo_gouv_2', 'none', '2018-10-22 21:40:56', 'no', 'no', 0, ''),
+(24, 'porchetta shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:02', 'no', 'no', 0, ''),
+(25, 'Fatback shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:03', 'no', 'no', 0, ''),
+(26, 'porchetta shank meatloaf porchetta venison pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:04', 'no', 'no', 0, ''),
+(27, 'porchetta shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:05', 'no', 'no', 0, ''),
+(28, 'Fatback shank meatloaf porchetta meatloaf pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:06', 'no', 'no', 0, ''),
+(29, 'porchetta shank meatloaf porchetta venison pork porchetta...', 'apo_gouv_2', 'none', '2018-10-22 21:40:07', 'no', 'no', 0, ''),
+(30, 'Fatback shank meatloaf porchetta venison pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:08', 'no', 'no', 0, ''),
+(31, 'Fatback meatloaf meatloaf porchetta venison pork loin...', 'apo_gouv_2', 'none', '2018-10-22 21:40:09', 'no', 'no', 0, ''),
+(32, 'meatloaf shank meatloaf meatloaf meatloaf pork loin...', 'apo_gouv_3', 'none', '2018-10-22 21:40:20', 'no', 'no', 0, ''),
+(33, 'Meatball pancetta salami leberkas fatback cupim. Kielbasa prosciutto jerky turkey bacon. Landjaeger drumstick ribeye turducken, pastrami venison short ribs.', 'mickey_mause', 'none', '2018-10-26 12:14:51', 'no', 'no', 0, ''),
+(34, 'Flank chuck short ribs, pork chop capicola venison pork belly pork. Pig prosciutto frankfurter, ribeye alcatra pancetta pork tri-tip.', 'mickey_mause', 'none', '2018-10-26 12:15:01', 'no', 'no', 0, ''),
+(35, 'Rump meatball flank alcatra kielbasa buffalo pork chop shankle pork chuck jerky venison beef ham hock.', 'mickey_mause', 'none', '2018-10-26 12:16:24', 'no', 'no', 0, ''),
+(36, 'Porchetta shank venison shoulder short ribs. Bresaola beef chicken rump filet mignon tenderloin jerky, pork chop burgdoggen beef ribs pork doner spare ribs. Tenderloin sausage bacon ham biltong beef ribs fatback ham hock pork loin.', 'goofy_duck', 'none', '2018-10-26 12:18:23', 'no', 'no', 1, ''),
+(37, 'Meatloaf beef tenderloin ball tip pig boudin spare ribs flank porchetta landjaeger sirloin tri-tip andouille cow brisket. Ham hock flank chuck, pork loin ham hamburger kielbasa boudin meatball. ', 'goofy_duck', 'none', '2018-10-26 12:18:35', 'no', 'no', 1, ''),
+(38, 'Filet mignon frankfurter kevin tongue tenderloin short ribs cupim meatball pig picanha doner tri-tip.', 'mike_ross', 'none', '2018-10-26 12:19:38', 'no', 'no', 0, ''),
+(39, 'Pork loin ham hock venison chicken ground round meatloaf tri-tip spare ribs tongue pastrami prosciutto filet mignon tenderloin short ribs meatball. Salami turkey ribeye beef biltong doner tenderloin. ', 'homer_simson', 'none', '2018-10-26 12:19:45', 'no', 'no', 0, ''),
+(40, 'Pancetta short ribs turkey, sausage pork loin alcatra pork chop cow spare ribs kevin. Pastrami hamburger beef ribs, shankle spare ribs biltong pig strip steak pancetta pork loin.', 'mike_ross', 'none', '2018-10-26 12:19:56', 'no', 'no', 1, ''),
+(41, 'Beef ribs strip steak bacon corned beef. Short ribs drumstick filet mignon ground round pork loin. ', 'homer_simson', 'none', '2018-10-26 12:20:05', 'no', 'no', 1, ''),
+(42, 'Pork loin prosciutto alcatra bacon spare ribs burgdoggen rump pig hamburger kevin.', 'mike_ross', 'none', '2018-10-26 12:20:13', 'no', 'no', 0, ''),
+(43, 'Shankle spare ribs pig jowl hamburger leberkas. Corned beef fatback short loin ham. Porchetta shankle drumstick short ribs pastrami. Andouille frankfurter filet mignon turkey shank buffalo.', 'homer_simson', 'none', '2018-10-26 12:20:24', 'no', 'no', 1, ''),
+(44, 'Testarious!\r\n', 'mike_ross', 'none', '2018-11-18 20:03:53', 'no', 'yes', 0, ''),
+(45, 'Hey mike!\r\n', 'goofy_duck', 'mike_ross', '2018-11-18 20:06:20', 'no', 'no', 0, ''),
+(46, 'Tha er8ei notice??', 'goofy_duck', 'none', '2018-12-15 20:17:18', 'no', 'no', 1, ''),
+(47, 'prfile postts', 'goofy_duck', 'none', '2018-12-15 20:18:50', 'no', 'no', 0, ''),
+(48, 'aaaaaaa 3c\r\n', 'goofy_duck', 'homer_simson', '2018-12-15 20:19:41', 'no', 'yes', 0, ''),
+(49, 'Hey Homer', 'goofy_duck', 'homer_simson', '2018-12-15 20:43:18', 'no', 'yes', 0, ''),
+(50, 'Hey Homer', 'goofy_duck', 'homer_simson', '2018-12-15 20:46:10', 'no', 'yes', 0, ''),
+(51, 'Hey Homer!', 'goofy_duck', 'homer_simson', '2018-12-15 20:47:51', 'no', 'yes', 0, ''),
+(52, 'aaaaaa No.5', 'goofy_duck', 'homer_simson', '2018-12-15 20:51:32', 'no', 'yes', 0, ''),
+(53, 'Hey Homer!', 'goofy_duck', 'homer_simson', '2018-12-15 20:52:25', 'no', 'no', 0, ''),
+(54, 'douleuei?? h mas douleuei??', 'goofy_duck', 'mike_ross', '2018-12-15 20:54:38', 'no', 'yes', 0, ''),
+(55, 'MIKE!!!!!!!!!!', 'goofy_duck', 'mike_ross', '2018-12-15 21:05:14', 'no', 'no', 0, ''),
+(56, 'hr8e to notice!!', 'homer_simson', 'goofy_duck', '2018-12-15 21:11:04', 'no', 'no', 0, ''),
+(57, '<br><iframe width=\\''420\\'' height=\\''315\\'' src=\\''https://www.youtube.com/embed/kCC8UIPrASA\\''></iframe><br>', 'goofy_duck', 'none', '2018-12-30 22:11:45', 'no', 'yes', 0, ''),
+(58, 'ROLF!!! Check this out! <br><iframe width=''420'' height=''315'' src=''https://www.youtube.com/embed/kCC8UIPrASA''></iframe><br>', 'goofy_duck', 'none', '2018-12-30 22:20:36', 'no', 'no', 0, ''),
+(59, 'Another one! <br><iframe width=''420'' height=''315'' src=''https://www.youtube.com/embed/gsWAdMJtWZs''></iframe><br>', 'goofy_duck', 'none', '2018-12-30 22:26:07', 'no', 'no', 0, ''),
+(60, 'Is anybody here?? When we will go for beers?', 'goofy_duck', 'none', '2018-12-30 22:56:14', 'no', 'no', 0, ''),
+(61, 'Hey guys, looking forward to superbowl!', 'goofy_duck', 'none', '2018-12-30 22:59:31', 'no', 'yes', 0, ''),
+(68, 'test002', 'goofy_duck', 'none', '2019-01-01 10:09:30', 'no', 'no', 0, 'assets/images/posts/5c2b203a25f45Star-Wars-The-Last-Jedi-1-3-600x641.jpg'),
+(69, 'test002', 'goofy_duck', 'none', '2019-01-01 10:10:49', 'no', 'yes', 0, 'assets/images/posts/5c2b2089c1a77Star-Wars-The-Last-Jedi-1-3-600x641.jpg'),
+(70, 'test002', 'goofy_duck', 'none', '2019-01-01 10:11:05', 'no', 'yes', 0, 'assets/images/posts/5c2b2099332ffStar-Wars-The-Last-Jedi-1-3-600x641.jpg'),
+(71, 'test002', 'goofy_duck', 'none', '2019-01-01 10:12:03', 'no', 'yes', 0, 'assets/images/posts/5c2b20d36c7a3Star-Wars-The-Last-Jedi-1-3-600x641.jpg'),
+(72, 'test002', 'goofy_duck', 'none', '2019-01-01 10:12:49', 'no', 'yes', 0, 'assets/images/posts/5c2b2101516b0Star-Wars-The-Last-Jedi-1-3-600x641.jpg'),
+(73, 'This is mike? :P hahah', 'goofy_duck', 'none', '2019-01-01 10:13:42', 'no', 'no', 0, 'assets/images/posts/5c2b21368ac98tourta_dia_de_los_muertos.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trends`
+--
+
+CREATE TABLE IF NOT EXISTS `trends` (
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hits` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `trends`
+--
+
+INSERT INTO `trends` (`title`, `hits`) VALUES
+('', 1),
+('Guys', 1),
+('Looking', 1),
+('Forward', 1),
+('Superbowl', 1),
+('Yffgh', 1),
+('Fdg', 1),
+('Test002', 4),
+('Mike', 1),
+('Hahah', 1);
 
 -- --------------------------------------------------------
 
@@ -285,7 +324,7 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `pass
 (2, 'Apo2', 'Gouv', 'apo_gouv_2', 'tester001@social-cube.gr', '$2y$12$HbIqgckB5yhbJQDYHj2VQug4cqTvU4GoZ1uk7MDksIxPVjj6m4BTy', '2018-10-20', 'http://localhost/friends-cube/assets/images/profile_pics/defaults/head_green_sea.png', 14, 0, 'no', ',mickey_mouse,homer_simson,'),
 (3, 'Apo3', 'Gouv', 'apo_gouv_3', 'tester002@social-cube.gr', '$2y$12$.CsIyUmZd3NX7kfIHsSrZudGllVb2tQLWDnvxSSCLbYWwjupgFlyG', '2018-10-20', 'http://localhost/friends-cube/assets/images/profile_pics/defaults/head_alizarin.png', 9, 0, 'no', ',goofy_duck,'),
 (10, 'Mickey', 'Mouse', 'mickey_mouse', 'mmouse@friendscube.gr', '$2y$12$Ivsm4xy0phPzVU0NZjxLZOJVF.tBiT/Hy4yAqoB.oMYq92EoGNvoO', '2018-10-26', 'http://localhost/friends-cube/assets/images/profile_pics/defaults/head_pomegranate.png', 3, 0, 'no', ',apo_gouv_2,goofy_duck,'),
-(11, 'Goofy', 'Duck', 'goofy_duck', 'goofy@friendscube.gr', '$2y$12$P.HbFMq8v52tDWmX0GGu7upMmvWkFZSoECwIfUOfwAtve6w3aOXGK', '2018-10-26', 'assets/images/profile_pics/goofy_duckb14536da7f960d1f0343c7ade933dd8bn.jpeg', 5, 3, 'no', ',mike_ross,apo_gouv,apo_gouv_3,mickey_mouse,homer_simson,'),
+(11, 'Goofy', 'Goof', 'goofy_duck', 'goofy@friendscube.gr', '$2y$12$P.HbFMq8v52tDWmX0GGu7upMmvWkFZSoECwIfUOfwAtve6w3aOXGK', '2018-10-26', 'assets/images/profile_pics/goofy_duckb14536da7f960d1f0343c7ade933dd8bn.jpeg', 21, 3, 'no', ',mike_ross,apo_gouv,apo_gouv_3,mickey_mouse,homer_simson,'),
 (12, 'Homer', 'Simson', 'homer_simson', 'hsimson@friendscube.gr', '$2y$12$bu7G6IcyLhxwKGf24AblUOk/TGG8eT0GPU1AVkPf.oogDTUZEvgci', '2018-10-26', 'http://localhost/friends-cube/assets/images/profile_pics/defaults/head_alizarin.png', 4, 2, 'no', ',apo_gouv_2,goofy_duck,'),
 (13, 'Mike', 'Ross', 'mike_ross', 'mross@friendscube.gr', '$2y$12$cWzco6240naa64ANWO9lSOkgoBsdaV1ZQe5r9v6AB.x5FvvgNzSAy', '2018-10-26', 'assets/images/profile_pics/mike_rossd2bfa2e7848386d8adfa845f7f78d989n.jpeg', 4, 1, 'no', ',goofy_duck,apo_gouv,');
 
@@ -370,7 +409,7 @@ ALTER TABLE `notifications`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=74;
 --
 -- AUTO_INCREMENT for table `users`
 --
